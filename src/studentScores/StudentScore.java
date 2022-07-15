4a22a2d4f01521da29de767eb1de2f5fdd987f77
Timmy <adeloduntimilehin15@gmail.com>
2022-07-15 14:04:46 +0100

    code

--- src/studentScores/StudentScore.java
+++ src/studentScores/StudentScore.java
@@ -1,2 +1,29 @@
-package studentScores;public class StudentScore {
+package studentScores;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentScore {
+    static ArrayList<String> student = new ArrayList<>();
+    static ArrayList<String> subject = new ArrayList<>();
+    static Scanner input = new Scanner(System.in);
+
+    static double total;
+    static double average;
+    static int position;
+
+    static String scanner() {
+        String info = input.nextLine();
+        return info;
+    }
+    static void scannerInput(){
+        System.out.printf("%s%n", "student");
+        String student = input.nextLine();
+
+
+
+}
+
+
+
 }
